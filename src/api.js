2696e22d0f3d94f19dc3952b1c51e2fefc1717cb
Benv1984 <47241499+Benv1984@users.noreply.github.com>
2022-10-07 09:42:27 +0300

    deploy my react app to github pages

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '24b85e0c62msh3c85874c1641f97p1aa449jsnc7183f91f25d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = 'a26931231de1a131db3cd57258e8ea24'
